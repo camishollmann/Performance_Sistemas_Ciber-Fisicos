62b5c3d33b9bc41302ca1e0939e6e6028f388502
Camilla Hollmann <camillahollmann@ufpr.br>
2025-03-09 19:38:15 -0300

    Codes from the class added.

---
+++ fibonacci_professor.py
@@ -0,0 +1,15 @@
+# Inicio:
+
+Valor1 = 0
+Valor2 = 1
+fibonacciAtual= 2
+
+# Repeticao:
+while fibonacciAtual != 10:
+    valor3 = Valor1 + Valor2
+    Valor1 = Valor2
+    Valor2 = valor3
+
+    fibonacciAtual += 1
+
+# Fim:
