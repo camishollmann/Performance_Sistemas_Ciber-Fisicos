9332ad8400f195d4732f4603dc3bbb3335e68b9e
Camilla Hollmann <camilla.hollmann@pucpr.edu.br>
2025-05-22 20:20:59 -0300

    Code started.

---
+++ conta_ocorrencias/Main.java
@@ -0,0 +1,24 @@
+package conta_ocorrencias;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Main {
+    public static ArrayList<String> listFilesFromFolder(String folder){
+        ArrayList<String> arquivos = new ArrayList();
+        File pasta = new File(folder);
+        File[] files = pasta.listFiles();
+
+        for (File file: files){
+            if (file.isFile()){
+                arquivos.add(file.getAbsolutePath());
+            }
+        }
+        return arquivos;
+    }
+}
+public static void main(String[] args) throws InterruptExceptions {
+    ArrayList<String> arquivos = listFilesFromFolder("/Users/");
+    System.out.println(arquivos);
+
+}
